0b36bbba910a08d4c4460348737987504eb45cf7
Can Türker <64265868+cantrkr@users.noreply.github.com>
2022-02-22 13:57:52 +0300

    Scr'ptable objects are created

---
+++ Assets/Project Data/Scripts/CharacterData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newChar",menuName = "Game Data/Create Character")]
+public class CharacterData : ScriptableObject
+{
+    public string charName;
+    public int age;
+    public string destination;
+
+    public List<ItemData> items;
+
+}
---
+++ Assets/Project Data/Scripts/ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newItem",menuName = "Game Data/Create Item")]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+
+}
